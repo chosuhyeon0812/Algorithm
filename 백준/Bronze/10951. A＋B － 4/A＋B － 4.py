0038d97138395f410684aa679fedaf20ac960c7d
chosuhyeon0812 <119795734+chosuhyeon0812@users.noreply.github.com>
2023-07-20 20:34:27 +0900

    [Bronze V] Title: A+B - 4, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -0,0 +1,14 @@
+while True:
+    # A, B = map(int, input().split())
+    # print(A + B)
+
+    # if (A == 0 & B == 0):
+    #     break
+
+    try: 
+        A, B = map(int, input().split())
+        print(A + B)
+    
+    except:
+        break
+    
