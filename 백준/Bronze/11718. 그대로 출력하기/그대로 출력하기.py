3e82687016d4a35b3e874d1f016900a822b66991
chosuhyeon0812 <119795734+chosuhyeon0812@users.noreply.github.com>
2023-07-27 00:41:52 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
@@ -0,0 +1,13 @@
+# for i in a:
+#     print(i)
+# s_list = []
+while True:
+    try:
+        print(input())
+    except EOFError:
+        break
+
+#     s_list.appent(s)
+
+# for i in s_list:
+#     print(i)
