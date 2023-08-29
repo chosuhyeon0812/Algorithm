1b16e5292a7b29be48d835a4ffadb527d4c76da9
chosuhyeon0812 <119795734+chosuhyeon0812@users.noreply.github.com>
2023-08-30 01:08:14 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 360 ms, Memory: 54868 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -7,10 +7,15 @@ result = []
 # x값에 상관없이 y 값을 정렬 => y 값 정렬
 for _ in range(N):
     x, y = map(int, input().split())
-    result.append([y, x])
+    result.append([x, y])
 
-result.sort()
-# print(result)
+# result.sort()
+# # print(result)
+#
+# for y, x in result:
+#     print(x, y)
 
-for y, x in result:
+
+ans = sorted(result, key = lambda x : (x[1], x[0]))
+for x, y in ans:
     print(x, y)
