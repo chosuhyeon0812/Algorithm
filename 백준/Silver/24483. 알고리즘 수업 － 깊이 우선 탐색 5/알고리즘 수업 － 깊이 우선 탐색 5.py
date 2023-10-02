fc0315800c025dafd5b2e1dccf5d3c6dd7d9f436
조수현 <119795734+chosuhyeon0812@users.noreply.github.com>
2023-10-02 22:12:45 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 5, Time: 576 ms, Memory: 74372 KB -BaekjoonHub

---
+++ 백준/Silver/24483. 알고리즘 수업 － 깊이 우선 탐색 5/알고리즘 수업 － 깊이 우선 탐색 5.py
@@ -0,0 +1,39 @@
+# 24483. 알고리즘 수업 - 깊이 우선 탐색5
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(1000000)
+
+def dfs(start, depth):
+    global order
+    visited[start] = depth
+    t[start] = order
+
+    for i in graph[start]:
+        if visited[i] == -1:
+            order += 1
+            dfs(i, depth+1)
+
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited = [-1] * (N+1)
+t = [0] * (N + 1)
+order = 1
+for _ in range(M):
+    s, e = map(int, input().split())
+    graph[s].append(e)
+    graph[e].append(s)
+
+for i in graph:
+    i.sort()
+
+dfs(R, 0)
+
+sum = 0
+for i in range(1, N+1):
+    if visited[i] == -1:
+        continue
+    sum += visited[i] * t[i]
+
+# print(t)
+# print(visited)
+print(sum)
