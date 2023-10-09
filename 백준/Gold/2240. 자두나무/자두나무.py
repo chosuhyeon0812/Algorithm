c79b3468f3469c5f2ae18aa9acc5a918f62a71b2
조수현 <119795734+chosuhyeon0812@users.noreply.github.com>
2023-10-10 02:05:47 +0900

    [Gold V] Title: 자두나무, Time: 80 ms, Memory: 33300 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.py
+++ 백준/Gold/2240. 자두나무/자두나무.py
@@ -1,25 +1,24 @@
-# 2240. 자두 나무
-
 T, W = map(int, input().split())
 tree = [int(input()) for _ in range(T)]
+plum = 0
 
 dp = [[[0] * 3 for _ in range(W + 2)] for _ in range(T+1)]
 if tree[0] == 1:
     dp[1][0][0] = 1
 else:
     dp[1][1][1] = 1
-
 for i in range(2, T+1):
-    trees = tree[i-1]
     for j in range(W + 1):
-        if trees == 1:
+        if tree[i-1] == 1:
             dp[i][j][0] = max(dp[i - 1][j][0], dp[i - 1][j - 1][1]) + 1
             dp[i][j][1] = max(dp[i - 1][j - 1][0], dp[i - 1][j][1])
         else:
-            dp[i][j][0] = max(dp[i - 1][j][0], dp[i - 1][j - 1][1])
+            if i == j == 1:
+                continue
+            dp[i][j][0] = max(dp[i - 1][j - 1][1], dp[i - 1][j][0])
             dp[i][j][1] = max(dp[i - 1][j - 1][0], dp[i - 1][j][1]) + 1
 
-plum = -1
-for w in range(W+1):
-    plum = max(plum, max(dp[T][w][0], dp[T][w][1]))
+plum = 0
+for i in dp[-1]:
+    plum = max(plum, max(i))
 print(plum)
