24db2fe7ccb851145af287a2843b2fbd13caf353
조수현 <119795734+chosuhyeon0812@users.noreply.github.com>
2023-10-18 22:13:55 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        
+        double sqrt = Math.sqrt(n);
+        
+        if(sqrt % 1 == 0) {
+            answer = (long) Math.pow(sqrt + 1, 2);
+            return answer;
+        
+        }
+        return -1;
+    }
+}
