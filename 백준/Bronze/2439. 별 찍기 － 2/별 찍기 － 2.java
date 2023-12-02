ce044d72b796fdf98592193b1c870eeac86f09b4
조수현 <119795734+chosuhyeon0812@users.noreply.github.com>
2023-12-02 19:09:02 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 392 ms, Memory: 23244 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,24 +1,20 @@
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
-		br.close();
-		
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N - i; j++) {
-				bw.write(' ');
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (j <= (n-i)) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
 			}
-			for (int k = 1; k <= i; k++) {
-				bw.write('*');
-			}
-			bw.newLine();
+			System.out.println();
+			sc.close();
 		}
-		bw.flush();
-		bw.close();
 	}
 }
