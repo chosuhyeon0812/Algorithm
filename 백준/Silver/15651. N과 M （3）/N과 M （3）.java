498ca387c2f08179a975c49fa4887f4b0aa2f54c
조수현 <119795734+chosuhyeon0812@users.noreply.github.com>
2024-01-29 19:18:11 +0900

    [Silver III] Title: N과 M (3), Time: 564 ms, Memory: 157836 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+
+        backtracking(0);
+        bw.flush();
+        bw.close();
+
+    }
+    private static void backtracking(int depth) throws IOException {
+        if(depth == m){
+            for (int i : arr) {
+                bw.write(i + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        for(int i = 1; i <= n; i++){
+                arr[depth] = i;
+                backtracking(depth+1);
+        }
+    }
+}
