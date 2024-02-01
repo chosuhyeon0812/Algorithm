1203828c3b04886868c246031fb26e083cf45286
조수현 <119795734+chosuhyeon0812@users.noreply.github.com>
2024-02-01 20:36:06 +0900

    [Silver III] Title: N과 M (6), Time: 148 ms, Memory: 15924 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static int[] result;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        result = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            result[i] =  Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(result);
+        backtracking(0, 0);
+        bw.flush();
+        bw.close();
+
+    }
+
+    private static void backtracking(int depth, int start) throws IOException {
+        if(depth == m){
+            for (int i : arr) {
+                bw.write(i + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+        for(int i = start; i < n; i++){
+            arr[depth] = result[i];
+            backtracking(depth+1, i+1);
+        }
+    }
+}
