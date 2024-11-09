1a9e09acf4e7cfc900ef0fd4154057195c177a43
조수현 <119795734+chosuhyeon0812@users.noreply.github.com>
2024-11-10 05:51:59 +0900

    [level 2] Title: 타겟 넘버, Time: 6.68 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -7,7 +7,11 @@ class Solution {
         // 모든 숫자를 다 사용했을 떄
         if(index == numbers.length) {
             // 목표 숫자와 합이 같으면 경우의 수 1 증가
-            return sum == target ? 1 : 0;
+            if (sum == target) {
+                return 1;
+            } else {
+                return 0;
+            }
         }
         // 현재 숫자를 더하는 경우와 빼는 경우로 재귀 호충..
         return dfs(numbers, target, index + 1, sum + numbers[index]) + dfs(numbers, target, index + 1, sum - numbers[index]);
